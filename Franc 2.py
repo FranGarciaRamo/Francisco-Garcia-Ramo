d33c47181edaf47a7bd00a13f2d021af0faf819f
FranGarciaRamo <71372579+FranGarciaRamo@users.noreply.github.com>
2020-09-24 12:56:29 +0200

    Add files via upload

---
+++ Franc 2.py
@@ -0,0 +1,14 @@
+def Franco_3():
+
+    NumeroFinal=input("Introduce hasta el numero")
+    SumaAcumulada=0
+    for cont in range(1,NumeroFinal+1):
+        if(cont%2==0):
+            SumaAcumulada=SumaAcumulada+cont
+    print "SumaAcumulada= ",SumaAcumulada
+       
+
+    
+   
+Franco_3()
+
